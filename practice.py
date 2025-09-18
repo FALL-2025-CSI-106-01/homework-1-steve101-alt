58a1d7059d395b1e5410b8940e44b110f0a6acc3
steve101-alt <skopinsky229@hcc.edu>
2025-09-18 16:30:35 +0000

    Homework 1 Done!

---
+++ practice.py
@@ -0,0 +1,28 @@
+# for number in range (3,11):
+#     print(number)
+
+# letters = ['a','b','c']
+# def print_abcs(letters):
+#     for letter in letters:
+#         print(letters)
+
+# print_abcs(letters)
+
+
+# var1 = "hello"
+
+# def my_function(var1):
+#   var1 = 'goodbye'
+#   return var1
+
+# #var1 = my_function(var1)
+# print(var1)
+
+
+var1 = "hello"
+
+def my_function(var1):
+  var1 = 'goodbye'
+
+var1 = my_function(var1)
+print(var1)
